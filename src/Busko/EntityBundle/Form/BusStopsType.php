68bbbd39ea2277625d0878132b9757c3e15bba23
dewmal345 <dewmal.anicitus@gmail.com>
2014-03-18 13:17:02 +0530

    bus stops finished

---
+++ src/Busko/EntityBundle/Form/BusStopsType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Busko\EntityBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class BusStopsType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('city')
+            ->add('stopId')
+            ->add('route')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Busko\EntityBundle\Entity\BusStops'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'busko_entitybundle_busstops';
+    }
+}
