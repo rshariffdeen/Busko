3014ef42845b4f135e25f978a6f2c8f2d7beb4da
dewmal345 <dewmal.anicitus@gmail.com>
2014-03-23 23:38:16 +0530

    add assistant add driver add operator add admin

--- src/Busko/EmployeeBundle/Controller/AdminController.php
+++ src/Busko/EmployeeBundle/Controller/AdminController.php
@@ -1,18 +1,28 @@
 <?php
 
 namespace Busko\EmployeeBundle\Controller;
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\UserBundle\FOSUserEvents;
 use FOS\UserBundle\Event\FormEvent;
-use FOS\UserBundle\Event\FilterUserResponseEvent;
 use FOS\UserBundle\Event\GetResponseUserEvent;
-use FOS\UserBundle\Model\UserInterface;
+use FOS\UserBundle\Event\UserEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\ContainerAware;
-
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Model\UserInterface;
+use Busko\EntityBundle\Form\EmployeePhonesType;
+use Busko\EntityBundle\Entity\EmployeePhones;
+use Doctrine\Common\Collections\ArrayCollection;
+use Busko\EntityBundle\Entity\Administrators;
+
+
+
+
+
+
 
 use Busko\EntityBundle\Entity\Employees;
 use Busko\EntityBundle\Form\EmployeeType;
@@ -180,6 +190,86 @@ class AdminController extends Controller
 
         return $this->redirect($this->generateUrl('site_emp', array('message' => "Oops! something went wrong",'type'=>'E')));
      }
+     
+     public function registerAction(Request $request) {
+        
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->container->get('fos_user.registration.form.factory');
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->container->get('fos_user.user_manager');
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->container->get('event_dispatcher');
+
+        $user = $userManager->createUser();
+        $phone = new EmployeePhones();
+        
+
+        $user->setEnabled(true);
+        $user->getPhone()->add($phone);
+
+        //$user->getDriver()->add($driver);
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+
+            $originalPhones = new ArrayCollection();
+
+            // Create an ArrayCollection of the current Tag objects in the database
+            foreach ($user->getPhone() as $tag) {
+                $originalPhones->add($tag);
+            }
+            $em = $this->container->get('doctrine')->getEntityManager();
+            if ($form->isValid()) {
+                foreach ($originalPhones as $tag) {
+                    $user->getPhone()->removeElement($tag);
+                }
+            }
+
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
+            $user->addRole('ADMIN');
+            $userManager->updateUser($user);
+            $admin=new Administrators();
+            foreach ($originalPhones as $tag) {
+                $tag->setId($user->getId());
+                $em->persist($tag);
+                $em->flush();
+            } 
+            $admin->setId($user);
+            $em->persist($admin);
+             $em->flush();
+
+            if (null === $response = $event->getResponse()) {
+                
+                $url = $this->container->get('router')->generate('site_emp',array('type'=>'S','message'=>'successfully added new Admin '));
+                $response = new RedirectResponse($url);
+            }
+            
+            
+
+            //  $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+            return $response;
+        }
+
+
+        return $this->container->get('templating')->renderResponse('BuskoEmployeeBundle:Administrators:register.html.' . $this->getEngine(), array(
+                    'form' => $form->createView(), 
+        ));
+    }
+    protected function getEngine() {
+        return $this->container->getParameter('fos_user.template.engine');
+    }
+
 
     /**
      * Creates a form to delete a Operators entity by id.
