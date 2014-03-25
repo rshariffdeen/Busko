3a276d782def6041334d108ae82f724ae3a7f9a5
Sabra Ossen <sabraosn@gmail.com>
2014-03-25 12:29:14 +0530

    pulled pulled pulled

--- src/Busko/BusBundle/Controller/RepairsController.php
+++ src/Busko/BusBundle/Controller/RepairsController.php
@@ -18,7 +18,11 @@ class RepairsController extends Controller
             return $this->forward('FOSUserBundle:Security:login');
         }
         $repair = new Repairs();
-        $form = $this->createForm(new RepairsType(), $repair);
+        $form = $this->createForm(new RepairsType(), $repair,array(
+            'attr' => array(
+                'class' => 'form-horizontal center'
+            )
+        ));
         $form->handleRequest($request);
         if ($form->isValid()) {
                 $repair = $form->getData();
@@ -31,6 +35,8 @@ class RepairsController extends Controller
                 catch(\Exception $e){
                     return $this->render('BuskoStyleBundle:Error:error.html.twig', array('message'=>'Check whether the repair is valid!'));
                 }
+                 return $this->forward('BuskoBusBundle:Buses:index',array('type'=>'S','message'=>'successfully added repair details to the bus')); 
+        
         }
           
          return $this->render('BuskoBusBundle:Repairs:addRepair.html.twig', array('form' => $form->createView())); 
@@ -40,10 +46,36 @@ class RepairsController extends Controller
        
     }
 
-
-    public function showRepairsAction($id)
+       public function showRepairsAction($id)
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('BuskoEntityBundle:Buses')->find($id);
         
+
+        if (!$entity) {
+             return $this->render('BuskoStyleBundle:Error:error.html.twig', array(
+                    'message' => ' bus could not be found'                   
+                    
+        ));}
+        /*add query to retrieve repairs belonging to this bus*/
+        $repository = $em->getRepository('BuskoEntityBundle:Repairs');
+        $query = $repository->createQueryBuilder('r')
+                    ->where('r.licNum = :title')
+                    ->setParameter('title', $id)
+                    ->getQuery();
+        $repairs = $query->getResult();
+        foreach($repairs as $r){
+            $date=$r->getStartDate();
+            /*Need to convert the string to date*/
+            
+            $r->setStartDate($date);
+        
+        }
+        /*send the selected repairs along with the template*/
+        return $this->render('BuskoBusBundle:Repairs:showRepairs.html.twig',array('repairs'=>$repairs,'id'=>$id));
     }
+	
+	
 
 }
